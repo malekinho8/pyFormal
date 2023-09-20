19eafda7461f32e7bf3e2bbccf679beb0237955a
malekinho8 <shmeek8@gmail.com>
2023-09-20 09:17:14 -0400

    updated setup.py to not include subprocess

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyFormal',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'pyperclip',
         'matplotlib',
-        'subprocess',
         'numpy',
         'click',
         'Pillow',  # Note: This is actually "Pillow" on PyPI
